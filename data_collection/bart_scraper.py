9481684fbb327be83865bf38741ce629d193654b
dmilad <dmilad@gmail.com>
2015-06-09 22:26:22 -0700

    commented, added more prints for error tracking.

--- data_collection/bart_scraper.py
+++ data_collection/bart_scraper.py
@@ -4,68 +4,118 @@ import time, datetime
 import object_storage
 from swift_cred import sl_storage
 
-def fetch():
+def fetch(api_urls):
+
+	"""
+	generate BART API commands, fetch the response for each, and write to appropriate text files. 
+	"""
+
 	api_key='MW9S-E7SL-26DU-VV8V'
-	with open('api_urls.txt', 'r') as urls:
+
+	#fetch response for all api commands listed in the api_urls file
+	with open(api_urls, 'r') as urls:
+		
+		#record fetch time (to be returned by the function and written to the appropriate fetchtime file)
 		fetchtime = datetime.datetime.today()
+		#record hour of the fetch (to use in file name)
 		fetchhour = fetchtime.hour
+		#record today's daye (to use in file name)
 		today = datetime.date.today()
+
+		#for each listed api command
 		for line in urls:
+
+			#grab name of command, and the url
 			filename, url = line.split('\t')
 			url = url.rstrip()
-			with open('data/'+filename+'_'+str(today)+'_'+str(fetchhour)+'.txt', 'a') as writefile:
-				response = requests.get(url+'&key='+api_key)
-				writefile.write(response.text+'\n\n')
-			print filename
-			print url+'&key='+api_key
+
+			#for each command, construct the appropriate file name containing the name of command, date, and hour of request
+			with open('data/' + filename + '_' + str(today) + '_' + str(fetchhour) + '.txt', 'a') as writefile:
+				response = requests.get(url + '&key=' + api_key)
+				writefile.write(response.text + '\n\n')
+			print str(fetchtime), filename, url + '&key=' + api_key
 	return fetchtime
 
-def send_to_storage():
-	with open('api_urls.txt', 'r') as urls:
+def send_to_storage(api_urls):
+
+	"""
+	sends 
+	"""
+
+	with open(api_urls, 'r') as urls:
+
+		#record the last fetch hour (so if now is 3pm, last hour was 2pm.)
 		fetchtime = datetime.datetime.today() - datetime.timedelta(hours = 1)
 		fetchhour = fetchtime.hour
+		#record last hour's date
 		today = (datetime.datetime.today() - datetime.timedelta(hours = 1)).date()
+
+		#for each listed api command
 		for line in urls:
+
+			#grab name of command
 			filename, url = line.split('\t')
 
-			fullfilename = 'data/'+filename+'_'+str(today)+'_'+str(fetchhour)+'.txt'
-			with open(fullfilename, 'r') as readfile:
-				sl_storage['bart_dump'][readfile.name.split('/')[-1]].send(readfile)
-		print 'Data files sent to object store.'
+			#construct last hour's file name using appropriate date and hour of request
+			fullfilename = 'data/' + filename + '_' + str(today) + '_' + str(fetchhour) + '.txt'
 
-		fullfilename = 'data/fetch_times_'+str(today)+'.txt'
+			#send the file to bart_dump container
+			with open(fullfilename, 'r') as readfile:
+				print "Sending " + fullfilename
+				sl_storage['bart_dump'][readfile.name.split('/')[-1]].send(readfile)
+
+		print 'All data files sent to object store.'
+
+		#contruct fetchtime file name associate with the last hour
+		fullfilename = 'data/fetch_times_' + str(today) + '.txt'
+		
+		#send the fetchtime file name, even if we are in still in the same day
 		with open(fullfilename, 'r') as readfile:
+			print "Sending " + fullfilename
 			sl_storage['bart_dump'][readfile.name.split('/')[-1]].send(readfile) 
+			
 		print 'Fetch times file sent to object store.'
 
 def start():
 	sent = False
 	while True:
+		#used to time 30 seconds between requests
 		start_time = time.time()
+
+		#call fetch, which makes api calls based on the urls provided in the api_urls files
+		#record the fetch time in the appropriate file, constructed using the date of fetch (one fetchfile per date)
 		try:
-			fetchtime = fetch()
-			with open('data/fetch_times_'+str(fetchtime.date())+'.txt', 'a') as fetchtimefile:
-				fetchtimefile.write(str(fetchtime)+'\n')
+			fetchtime = fetch('api_urls.txt')
+			with open('data/fetch_times_' + str(fetchtime.date()) + '.txt', 'a') as fetchtimefile:
+				fetchtimefile.write(str(fetchtime) + '\n')
 		except:
 			pass
+
+		#in the first minute of every hour, send files to object storage. 
+		#the purpose of the "sent" boolean to make sure we dont send same files twice in the first minute
 		try:
 			if fetchtime.minute == 0 and not sent:
-				send_to_storage()
+				send_to_storage('api_urls.txt')
 				sent = True
 			else:
 				sent = False
 		except Exception, e:
-			print "An error occurred. Files could not be sent to object store.", e
+			print "An error occurred. Files could not be sent to object store at " + str(fetchtime), e
 
+		#time 30 seconds between requests
 		end_time = time.time()
 		duration = end_time - start_time 
 		time.sleep(max([0, 30 - duration]))
 
 if __name__ == '__main__':
+
+	#run this forever
 	while True:
+
+		#if there is an error, try again in 60 seconds
 		try:
 			start()
 		except Exception, e:
-			print "error: ", e
-			print "retrying in 60 seconds..."
+			print "Error: ", e
+			print "Retrying in 60 seconds..."
 		time.sleep(60)
--- data_collection/bulk_send.py
+++ data_collection/bulk_send.py
@@ -3,23 +3,41 @@ import os
 import re
 
 def to_send(maindir):
+	
+	"""
+	figure out which files to send, and return a list containing the names of those files.
+	"""
+	
+	#grab list of files locally
 	here = [f for f in os.walk(maindir).next()[2] if f.endswith(".txt")]
+	#grab list of all fetch files locally
 	fetchhere = [f for f in os.walk(maindir).next()[2] if f.startswith("fetch")]
 
+
 	there = []
+	#grab list of remote files as objects
 	there_pre = sl_storage['bart_dump'].objects()
 
+	#convert objects to string, add to "there"
 	for pre in there_pre:
 		prestr = pre.__str__()
 		match = re.search('bart_dump, (.*)\.txt', prestr)
-		there.append(match.group(1)+'.txt')
+		there.append(match.group(1) + '.txt')
 
+	#create list of files to send: all files here that are not there, but including all fetch files
 	to_send = list(set(list(set(here) - set(there)) + fetchhere))
+	print "Number of files to send:", str(len(to_send))
 	return to_send
 
 
 def bulk_send(maindir, to_send):
-	for item in to_send:
+	
+	"""
+	send files one by one to remote object store.
+	"""
+
+	for i, item in enumerate(to_send):
+		print "Sending file " + str(i + 1) + ": " + item
 		fullfilename = maindir + '/' + item
 		with open(fullfilename, 'r') as readfile:
 			sl_storage['bart_dump'][readfile.name.split('/')[-1]].send(readfile)
@@ -27,4 +45,4 @@ def bulk_send(maindir, to_send):
 
 
 if __name__ == '__main__':
-	bulk_send('data', to_send('data'))
+	bulk_send(maindir = 'data', to_send = to_send('data'))
