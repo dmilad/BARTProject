c7dfe775a3098e43a16f19a669d90401d04679ed
dmilad <dmilad@gmail.com>
2015-06-28 20:51:49 -0700

    troubleshoot 5.5

--- processing/test.py
+++ processing/test.py
@@ -32,23 +32,23 @@ def trans():
 
 	print fts
 
-#	#for each fetchtime
-#	for ft in fts:
-#		print "Reading " + ft
-#		#read fetchtime file from object_store and write to to_parse
-#		newLocalFile = open('to_parse/'+ft, "w")
-#		swiftObject = sl_storage[archive_container][ft].read()
-#		newLocalFile.write(swiftObject)
-#		newLocalFile.close()
-#
-#		to_send = fts
-#
-#		print "Sending files for archive container..."
-#		for i, item in enumerate(to_send):
-#			print "Sending file " + str(i + 1) + ": " + item
-#			fullfilename = 'to_parse/' + item
-#			with open(fullfilename, 'r') as readfile:
-#				sl_storage[dump_container][readfile.name.split('/')[-1]].send(readfile)
-#		print 'Data files sent to object store.'
+	#for each fetchtime
+	for ft in fts:
+		print "Reading " + ft
+		#read fetchtime file from object_store and write to to_parse
+		newLocalFile = open('to_parse/'+ft, "w")
+		swiftObject = sl_storage[archive_container][ft].read()
+		newLocalFile.write(swiftObject)
+		newLocalFile.close()
+
+		to_send = fts
+
+		print "Sending files for archive container..."
+		for i, item in enumerate(to_send):
+			print "Sending file " + str(i + 1) + ": " + item
+			fullfilename = 'to_parse/' + item
+			with open(fullfilename, 'r') as readfile:
+				sl_storage[dump_container][readfile.name.split('/')[-1]].send(readfile)
+		print 'Data files sent to object store.'
 
 trans()
