398fa4a17851f49666c5ef8788118a6fff585688
dmilad <dmilad@gmail.com>
2015-07-15 21:28:36 -0700

    change grab_load

--- data_collection/grab_load.py
+++ data_collection/grab_load.py
@@ -8,6 +8,8 @@ url = 'http://api.bart.gov/api/sched.aspx?cmd=load'
 ssss = ['SHAY', 'FTVL', 'NCON', 'COLS', 'WCRK', 'WOAK', 'CONC', 'CAST', 'ORIN', 'SFIA', 'WDUB', 'DALY', 'EMBR', 'CIVC', 'SBRN', 'HAYW', 'LAFY', 'DBRK', '12TH', 'PHIL', 'PITT', 'NBRK', 'MONT', 'POWL', 'ROCK', 'SANL', '16TH', 'FRMT', 'BALB', 'DELN', 'MCAR', 'DUBL', '24TH', 'PLZA', 'MLBR', 'GLEN', 'BAYF', 'LAKE', 'ASHB', 'UCTY', '19TH', 'COLM', 'RICH', 'SSAN', 'OAKL']
 
 rr = [1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 19, 20]
+
+
 rr = [str(i) for i in rr]
 for i, r in enumerate(rr):
 	if len(r) == 1:
@@ -33,7 +35,6 @@ for a in ssss:
 				print 'yes!'
 				loads.append([sched_id, a, b, c, load])
 
-with open('load.txt', 'a') as writefile:
-	writefile.write(loads)
-
-print loads
+with open('load.txt', 'w') as writefile:
+	for l in loads:
+		writefile.writelines("\t".join(v for v in l)+"\n")
